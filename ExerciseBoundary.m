44fbeca6181d60b5d49f4b25bd4768c67a2a4e65
Tim Murray <Tim Murray>
2017-04-15 17:48:37 -0500

    All code, including ExerciseBoundary.m, Working correctly

--- ExerciseBoundary.m
+++ ExerciseBoundary.m
@@ -2,7 +2,7 @@ function [ price,S ] = ExerciseBoundary(Option,K,T,sigma,r,q,N,Exercise,ub,lb)
 %ExerciseBoundary: searches for S*(T) using binomial search. stops when the
 %region of interest is less than bound.
     
-    bound = 5*10^-4;
+    bound = 5*10^-3;
     %disp(K);
     OpType = 1;
     if (Option=='C')
@@ -17,14 +17,16 @@ function [ price,S ] = ExerciseBoundary(Option,K,T,sigma,r,q,N,Exercise,ub,lb)
        % disp([num2str(K)]);
         [price,ct] = Binomial(Option,K,T,S,sigma,r,q,N,Exercise);
         disp([S,price,ct]);
+        %disp(OpType*(K-S));
+        %disp(price == OpType*(K-S));
         %disp(OpType*(K-S)+ bound);
         %[exer,index] = ExerciseAt(Option,S,K,price);
         % disp(['Cycle: ',num2str(count),' Candidate Solution: ',num2str(S)]);
 
-        if( ub-lb < bound/10 || abs(OpType*(K-S)-price) <= bound)
+        if( ub-lb < bound/10)
             go = false;
-        elseif ( OpType*(K-S)+ bound >= price)
-            if (OpType ==1)
+        elseif ( OpType*(K-S)+bound >= price)
+            if (OpType ==1) %Put
                 lb = S;
                 S = (ub+lb)/2;
             else
@@ -39,6 +41,7 @@ function [ price,S ] = ExerciseBoundary(Option,K,T,sigma,r,q,N,Exercise,ub,lb)
             S = (ub+lb)/2;
         end
     end
+    price = -OpType*(price - OpType*K);
     
 end
 
